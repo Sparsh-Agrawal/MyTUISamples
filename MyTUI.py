14fa28684082b7edabfb6f62b464e4aa003fe0b8
Sparsh-Agrawal <44096686+Sparsh-Agrawal@users.noreply.github.com>
2020-04-02 18:07:07 +0530

    Update MyTUI.py

--- MyTUI.py
+++ MyTUI.py
@@ -1,24 +1,28 @@
-elif c==3:
-    os.system("ls -f")
-    print()
-elif c==4:
-    print("Enter file name : ",end="")
-    f=input()
-    os.system("touch "+f)
-    print("File successfully created")
-elif c==5:
-    os.system(live)
-    #print("Exiting...")
-    #os.system("sleep 2")
-    #exit()
-else:
-    print("Next time choose wisely...")
-    os.system(live)
-    #print("Exiting in ")
-    #exit()
+import os
 
-os.system("tput setaf 7")
-exit()
+os.system("clear")
+os.system("tput setaf 1")
 
+print("\t\t\t Welcome to my TUI")
+print("\t\t\t*-----------------*")
 
-                                                              63,0-1        Bot
+os.system("tput setaf 4")
+
+print("""
+Press 1 : To see currrent date and time
+Press 2 : To see Calander
+Press 3 : To list files
+Press 4 : To create file
+Press 5 : To exit
+""")
+
+os.system("tput setaf 3")
+
+live="""a=3
+while [ $a -ge 0 ]
+do
+echo -ne "Exiting...$a`sleep 1`\b\b\b\b\b\b\b\b\b\b\b"
+a=$((a-1))
+done
+exit
+                                                                     1,4           Top
